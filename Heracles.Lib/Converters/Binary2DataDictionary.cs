162aebd5a71f7c9c900c08c20578ca9bdfcf2f1b
Darkc0m <52318740+Darkc0m@users.noreply.github.com>
2023-05-07 23:48:19 +0200

    DataDictionary format and converters

---
+++ Heracles.Lib/Converters/Binary2DataDictionary.cs
@@ -0,0 +1,53 @@
+﻿using Heracles.Lib.Formats;
+using Heracles.Lib.Utils;
+using Yarhl.FileFormat;
+using Yarhl.IO;
+
+namespace Heracles.Lib.Converters
+{
+    public class Binary2DataDictionary : IConverter<BinaryFormat, DataDictionary>, IConverter<DataDictionary, BinaryFormat>
+    {
+        public DataDictionary Convert(BinaryFormat bin) {
+            var reader = new HeraclesReader(bin.Stream);
+            var data = new DataDictionary();
+
+            data.numEntries = reader.ReadUInt32();
+            for (int i = 0; i < data.numEntries * 11 + 1; i++) {
+                data.text.Add(reader.ReadPointer32String());
+            }
+
+            return data;
+        }
+
+        public BinaryFormat Convert(DataDictionary data) {
+            var bin = new BinaryFormat();
+            var writer = new HeraclesWriter(bin.Stream);
+
+            writer.Write(data.numEntries);
+
+            uint currentOffset = sizeof(uint) + sizeof(uint) * (data.numEntries * 11 + 1);
+            for (int i = 0; i < data.numEntries; i++) {
+                writer.Write(currentOffset);
+                currentOffset += 0x3E;
+                for (int j = 0; j < 10; j++) {
+                    writer.Write(currentOffset);
+                    currentOffset += (uint)writer.GetByteCount(data.text[i * 11 + j + 1]);
+                }
+            }
+            writer.Write(currentOffset);
+            int id;
+            for (int i = 0; i < data.numEntries; i++) {
+                id = i * 11;
+                writer.Write(data.text[id], 0x3E);
+                for (int j = 0; j < 10; j++) {
+                    id = i * 11 + j + 1;
+                    writer.Write(data.text[id]);
+                }
+            }
+            writer.Write(data.text[^1]);
+            writer.Write((byte)0x00);
+
+            return bin;
+        }
+    }
+}
---
+++ Heracles.Test/DataDictionaryFormatTest.cs
@@ -0,0 +1,74 @@
+﻿using Heracles.Lib.Converters;
+using Heracles.Lib.Formats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yarhl.FileSystem;
+using Yarhl.IO;
+using Yarhl.Media.Text;
+
+namespace Heracles.Test
+{
+    public class DataDictionaryFormatTest
+    {
+        private string resPath;
+        [SetUp]
+        public void Setup() {
+            string programDir = AppDomain.CurrentDomain.BaseDirectory;
+            resPath = Path.GetFullPath(programDir + "/../../../Resources/DataDictionary.eng");
+
+            Assert.True(File.Exists(resPath), "The resource file does not exist", resPath);
+        }
+
+        [Test]
+        public void DataDictionaryTest() {
+            using (var node = NodeFactory.FromFile(resPath)) {
+                // BinaryFormat -> DataDictionary
+                var expectedBin = node.GetFormatAs<BinaryFormat>();
+                var binary2DataDictionary = new Binary2DataDictionary();
+                DataDictionary expectedDataDictionary = null;
+                try {
+                    expectedDataDictionary = binary2DataDictionary.Convert(expectedBin);
+                }
+                catch (Exception ex) {
+                    Assert.Fail($"Exception BinaryFormat -> DataDictionary with {node.Path}\n{ex}");
+                }
+
+                // DataDictionary -> Po
+                var DataDictionary2Po = new DataDictionary2Po();
+                Po expectedPo = null;
+                try {
+                    expectedPo = DataDictionary2Po.Convert(expectedDataDictionary);
+                }
+                catch (Exception ex) {
+                    Assert.Fail($"Exception DataDictionary -> Po with {node.Path}\n{ex}");
+                }
+
+                //new Po2Binary().Convert(expectedPo).Stream.WriteTo(AppDomain.CurrentDomain.BaseDirectory + "/../../../" + "Resources/DataDictionary.po");
+
+                // Po -> DataDictionary
+                DataDictionary actualDataDictionary = null;
+                try {
+                    actualDataDictionary = DataDictionary2Po.Convert(expectedPo);
+                }
+                catch (Exception ex) {
+                    Assert.Fail($"Exception Po -> DataDictionary with {node.Path}\n{ex}");
+                }
+
+                // DataDictionary -> BinaryFormat
+                BinaryFormat actualBin = null;
+                try {
+                    actualBin = binary2DataDictionary.Convert(actualDataDictionary);
+                }
+                catch (Exception ex) {
+                    Assert.Fail($"Exception DataDictionary -> BinaryFormat with {node.Path}\n{ex}");
+                }
+
+                // Comparing Binaries
+                Assert.True(expectedBin.Stream.Compare(actualBin.Stream), $"DataDictionary is not identical: {node.Path}");
+            }
+        }
+    }
+}
