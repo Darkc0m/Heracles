29117b4aad14b7e046a50941b820df262088fc94
Darkc0m <52318740+Darkc0m@users.noreply.github.com>
2023-05-19 13:47:25 +0200

    CLI initial commit

--- Heracles.CLI/Program.cs
+++ Heracles.CLI/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Heracles.CLI
+﻿using System.CommandLine;
+
+namespace Heracles.CLI
 {
-    internal class Program
+    public static class Program
     {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+        public static int Main(string[] args) {
+            return new RootCommand("Export and import Glory of Heracles (NDS) files") {
+                new Command("export-t", "Export text from different format files into .po files")
+                {
+                    new Command("arm", "Export text from an arm9.bin file into multiple .po files")
+                },
+                new Command("import-t", "Import text from .po files into their original file format")
+                {
+                    new Command("arm", "Import text from .po files into an arm9.file (All .po files must be present!!!)")
+                }
+            }.Invoke(args);
         }
     }
 }
