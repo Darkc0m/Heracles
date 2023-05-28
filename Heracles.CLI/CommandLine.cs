a6858a44679eacaedd1051ff3ff2901fbfaecf93
Darkc0m <52318740+Darkc0m@users.noreply.github.com>
2023-05-28 18:16:15 +0200

    Fix wrong command info on import text commnads

--- Heracles.CLI/CommandLine.cs
+++ Heracles.CLI/CommandLine.cs
@@ -73,52 +73,52 @@ namespace Heracles.CLI
         }
 
         public static Command ImportTextCommand() {
-            var importScedic = new Command("scedic", "Export text from a SCEDIC.bin file into a .po file")
+            var importScedic = new Command("scedic", "Import text from a .po file to a SCEDIC.bin file")
             {
-                new Option<string>("--srcPath", "Path of the SCEDIC.bin file", ArgumentArity.ExactlyOne),
-                new Option<string>("--dirPath", "The directory where the .po file will be saved", ArgumentArity.ExactlyOne),
+                new Option<string>("--srcPath", "Path of the .po file", ArgumentArity.ExactlyOne),
+                new Option<string>("--dirPath", "The directory where the SCEDIC.bin file will be saved", ArgumentArity.ExactlyOne),
             };
             importScedic.Handler = CommandHandler.Create<string, string>(TextCommands.importScedic);
 
-            var importUIMess = new Command("uimess", "Export text from a UIMess.bin file into a .po file (do not confuse with UIMess.arc)")
+            var importUIMess = new Command("uimess", "Import text from a .po file to a UIMess.bin file (do not confuse with UIMess.arc)")
             {
-                new Option<string>("--srcPath", "Path of the UIMess.bin file", ArgumentArity.ExactlyOne),
-                new Option<string>("--dirPath", "The directory where the .po file will be saved", ArgumentArity.ExactlyOne),
+                new Option<string>("--srcPath", "Path of the .po file", ArgumentArity.ExactlyOne),
+                new Option<string>("--dirPath", "The directory where the UIMess.bin file will be saved", ArgumentArity.ExactlyOne),
             };
             importUIMess.Handler = CommandHandler.Create<string, string>(TextCommands.importUIMess);
 
-            var importDataDic = new Command("dic", "Export text from a DataDictionary.bin file into a .po file")
+            var importDataDic = new Command("dic", "Import text from a .po file to a DataDictionary.bin file")
             {
-                new Option<string>("--srcPath", "Path of the DataDictionary.eng file", ArgumentArity.ExactlyOne),
-                new Option<string>("--dirPath", "The directory where the .po file will be saved", ArgumentArity.ExactlyOne),
+                new Option<string>("--srcPath", "Path of the .po file", ArgumentArity.ExactlyOne),
+                new Option<string>("--dirPath", "The directory where the DataDictionary.eng file will be saved", ArgumentArity.ExactlyOne),
             };
             importDataDic.Handler = CommandHandler.Create<string, string>(TextCommands.importDataDictionary);
 
-            var importItem = new Command("item", "Export text from an itemdata.bin file into a .po file")
+            var importItem = new Command("item", "Import text from a .po file to an itemdata.bin file")
             {
-                new Option<string>("--srcPath", "Path of the itemdata.bin file", ArgumentArity.ExactlyOne),
-                new Option<string>("--dirPath", "The directory where the .po file will be saved", ArgumentArity.ExactlyOne),
+                new Option<string>("--srcPath", "Path of the .po file", ArgumentArity.ExactlyOne),
+                new Option<string>("--dirPath", "The directory where the itemdata.bin file will be saved", ArgumentArity.ExactlyOne),
             };
             importItem.Handler = CommandHandler.Create<string, string>(TextCommands.importItemdata);
 
-            var importMons = new Command("mons", "Export text from a monsdata_e.bin file into a .po file")
+            var importMons = new Command("mons", "Import text from a .po file to a monsdata_e.bin file")
             {
-                new Option<string>("--srcPath", "Path of the monsdata_e.bin file", ArgumentArity.ExactlyOne),
-                new Option<string>("--dirPath", "The directory where the .po file will be saved", ArgumentArity.ExactlyOne),
+                new Option<string>("--srcPath", "Path of the .po file", ArgumentArity.ExactlyOne),
+                new Option<string>("--dirPath", "The directory where the monsdata_e.bin file will be saved", ArgumentArity.ExactlyOne),
             };
             importMons.Handler = CommandHandler.Create<string, string>(TextCommands.importMonsdata);
 
-            var importTutorial = new Command("tut", "Export text from a tutorial.bin file into a .po file (extracted from a .arc file)")
+            var importTutorial = new Command("tut", "Import text from a .po file to a tutorial.bin file (extracted from a .arc file)")
             {
-                new Option<string>("--srcPath", "Path of the tutorial.bin file", ArgumentArity.ExactlyOne),
-                new Option<string>("--dirPath", "The directory where the .po file will be saved", ArgumentArity.ExactlyOne),
+                new Option<string>("--srcPath", "Path of the .po file", ArgumentArity.ExactlyOne),
+                new Option<string>("--dirPath", "The directory where the tutorial.bin file will be saved", ArgumentArity.ExactlyOne),
             };
             importTutorial.Handler = CommandHandler.Create<string, string>(TextCommands.importTutorial);
 
-            var importArm = new Command("arm", "Export text from an arm9.bin file into .po files")
+            var importArm = new Command("arm", "Import text from .po files to a arm9.bin file")
             {
-                new Option<string>("--srcPath", "Path of the itemdata.bin file", ArgumentArity.ExactlyOne),
-                new Option<string>("--dirPath", "The directory where the .po files will be saved", ArgumentArity.ExactlyOne),
+                new Option<string>("--srcPath", "Path with the .po files", ArgumentArity.ExactlyOne),
+                new Option<string>("--dirPath", "The directory where the arm9.bin file will be saved", ArgumentArity.ExactlyOne),
             };
             importArm.Handler = CommandHandler.Create<string, string>(TextCommands.importArm);
 
